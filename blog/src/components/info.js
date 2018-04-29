d0991c608cbe3e50532ebc07ba0a602f727b1b16
chenglei <chenglei@mistong.com>
2018-04-29 22:49:34 +0800

    修改个人信息部分的css

--- blog/src/components/info.js
+++ blog/src/components/info.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import '../css/info.css'
 import Avatar from '../resource/avatar.png'
 import Friends from '../components/fridens'
+import '../css/friends.css'
 
 export default class Info extends Component {
     render() {
@@ -10,6 +11,8 @@ export default class Info extends Component {
                 <img src={Avatar} className='avatar' alt='博主的头像'/>
                 <p className='username'>程磊</p>
                 <p className='usersign'><u>Android程序员，擅长搬砖</u></p>
+                <br/>
+                <p className='friend_a'>Friends</p>
                 <Friends className='userflinks'/>
             </div>
         )
