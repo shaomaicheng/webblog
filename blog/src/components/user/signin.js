56b72c608ba482862b54c69295a8ebeeb55c0007
chenglei <chenglei@mistong.com>
2018-09-15 00:15:33 +0800

    添加一部分注册页面的输入框

--- blog/src/components/user/signin.js
+++ blog/src/components/user/signin.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 import '../../css/user/signin.css'
+import {Input, Col, Spin,  message} from 'antd'
+
+const InputGroup = Input.Group;
 
 class SignInComponent extends Component {
 
@@ -9,8 +12,18 @@ class SignInComponent extends Component {
 
     render() {
         return (
-            <div className='signInDiv'>
-
+            <div className='signin'>
+                <h1 className='signinTitle'>注册</h1>
+                <div className='signinDiv'>
+                    <InputGroup size='large'>
+                        <Col className='signinCol'>
+                            <Input placeholder={'请输入用户名'} />
+                            <Input placeholder={'请输入手机号码'} />
+                            <Input placeholder={'请输入密码'} />
+                            <Input placeholder={'再起输出密码'} />
+                        </Col>
+                    </InputGroup>
+                </div>
             </div>
         )
     }
